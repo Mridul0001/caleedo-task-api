3c20ee74d4de34ed55985a65333cbcfd868061d1
Mridul0001 <mridulmahajan0001@gmail.com>
2022-01-02 17:12:27 +0530

    caleedo task api project

--- src/main/java/com/caleedo/api/aqidate/AQIDateController.java
+++ src/main/java/com/caleedo/api/aqidate/AQIDateController.java
@@ -5,10 +5,7 @@ import com.caleedo.api.models.CustomerModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +14,7 @@ import java.util.List;
 public class AQIDateController {
     @Autowired AQIDateService aqiDateService;
     @PostMapping("/addvalue")
-    public ResponseEntity addNewValue(AQIDateModel aqiDateModel){
+    public ResponseEntity addNewValue(@RequestBody AQIDateModel aqiDateModel){
         try{
             aqiDateService.addNewValue(aqiDateModel);
             return new ResponseEntity(HttpStatus.ACCEPTED);
